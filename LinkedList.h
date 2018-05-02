25f5173686143d05c89ee7cd98a71fcbdc55972b
jblaney1 <35744874+jblaney1@users.noreply.github.com>
2018-05-01 21:35:37 -0600

    Add files via upload

---
+++ LinkedList.h
@@ -0,0 +1,260 @@
+// LinkedList.h
+
+// tom bailey   0740  5 oct 2010
+// Declaration of the List class.
+
+// A List stores and manages a sequence of doubles.
+
+// This version uses a pointer to the first Node of a
+//   singly linked list of Nodes.
+
+#include <iostream>
+using std::ostream;
+using std::cout;
+
+class Node
+{
+public:
+	std::string entry_;
+	Node * next_;
+
+public:
+	// Constructors
+
+	// post: this Node contains entry and a NULL pointer
+	explicit Node(std::string entry);
+
+	// post: this Node contains entry and next
+	Node(std::string entry, Node * next);
+
+	// Destructor
+
+	~Node();
+
+private:
+	// Inaccessible standard functions
+	Node();
+	Node(const Node &);
+	const Node & operator=(const Node &);
+};
+
+Node::Node(std::string entry)
+	: entry_(entry), next_(NULL) {}
+
+Node::Node(std::string entry, Node * next)
+	: entry_(entry), next_(next) {}
+
+Node::~Node() {}
+
+#define ulong unsigned long
+
+
+
+
+// LinkedList.cpp
+
+// tom bailey   0745  5 oct 2010
+// Definition of methods for the List class.
+
+// MODIFIED: by rafe cooley 11/13/2017
+// change entry type to string for hashing lab
+
+#include <string>
+using std::string;
+
+#define ulong unsigned long
+
+
+class List
+{
+public:
+	//*** Standard functions ***
+
+	// Default constructor
+	// post: this List is empty.
+	List();
+
+	// Copy constructor
+	// post: this List now stores a copy of the sequence stored
+	//         in other
+	List(const List & other);
+
+	// Destructor
+	// post: all the Nodes of this List have been destroyed.
+	~List();
+
+	// Assignment operator
+	// post: this List now stores a copy of the sequence stored
+	//         in other
+	const List & operator=(const List & other);
+
+
+	//*** Accessors ***
+
+	// post: true has been returned just if this List is
+	//         empty.
+	bool empty() const;
+
+	// post: the doubles in this List have been written to
+	//         outfile.
+	void print() const;
+
+
+	//*** Mutators ***
+
+	// post: x has been added as the first double in this
+	//         List.
+	void insertAsFirst(string x);
+
+	void insertAsLast(string x);
+	// pre:  this List is not empty.
+	// post: the first double in this List has been removed
+	//         from this List and has been returned.
+	string removeFirst();
+
+private:
+	//*** Inaccessible standard functions ***
+
+
+	//*** Helper functions ***
+	// process part of the linked structure
+
+	// post: a pointer to a copy of the linked structure
+	//         targeted by ptr has been returned.
+	static Node * clone(Node * ptr);
+
+
+private:
+	Node * first_;
+
+};
+
+
+List::List()
+	: first_(NULL)
+{
+}
+
+
+List::List(const List & other)
+	: first_(clone(other.first_))
+{
+}
+
+
+List::~List()
+{
+	while (!empty())
+	{
+		removeFirst();
+	}
+}
+
+
+const List & List::operator=(const List & other)
+{
+	// check for list = list
+	if (&other != this)
+	{
+		// clear the current contents of this List
+		this -> ~List();
+		// and get a copy of other
+		first_ = clone(other.first_);
+	}
+
+	return *this;
+}
+
+
+bool List::empty() const
+{
+	return first_ == NULL;
+}
+
+
+void List::insertAsFirst(string x)
+{
+	first_ = new Node(x, first_);
+}
+
+void List::insertAsLast(string x)
+{
+	if (!empty())
+	{
+		Node * ptr = first_;
+		while (ptr != NULL)
+		{
+			if (ptr->next_ == NULL)
+			{
+				ptr->next_ = new Node(x);
+				break;
+			}
+			else { ptr = ptr->next_; }
+		}
+	}
+	else { insertAsFirst(x); }
+}
+
+string List::removeFirst()
+{
+	string item = first_->entry_;
+	Node * tempPtr = first_;
+	first_ = first_->next_;
+	delete tempPtr;
+	return item;
+}
+
+
+void List::print() const
+{
+	cout << "[ ";
+	if (!empty())
+	{
+		// The first entry is printed separately because no comma
+		// is needed.
+		cout << first_->entry_;
+		Node * ptr = first_->next_;
+		while (ptr != NULL)
+		{
+			cout << " | " << ptr->entry_;
+			ptr = ptr->next_;
+		}
+	}
+	cout << " ]";
+}
+
+
+// Iterative version of clone.
+// This version walks down the linked structure making a
+//   new Node for each double in the structure.
+Node* List::clone(Node * ptr)
+{
+	if (ptr == NULL)
+	{
+		return NULL;
+	}
+	Node * first = new Node(ptr->entry_);
+	Node * last = first;
+	ptr = ptr->next_;
+	while (ptr != NULL)
+	{
+		last->next_ = new Node(ptr->entry_);
+		last = last->next_;
+		ptr = ptr->next_;
+	}
+	return first;
+}
+
+
+//// Recursive version of clone.
+//// This version handles two cases, a linked structure with
+////   no Nodes, and a linked structure with one or more
+////   Nodes.
+//Node * List::clone( Node * ptr )
+//{
+//   if( ptr == NULL )
+//   {
+//      return NULL;
+//   }
+//   return new Node( ptr->entry_, clone( ptr->next_ ) );
+//}
--- Sort.h
+++ Sort.h
@@ -2,9 +2,7 @@
 #include <iterator>
 #include <iostream>
 #include <string>
-
-#include "LinkedList.h"
-
+#include "LinkedList.h" // From Lab2
 using std::cout;
 using std::endl;
 
@@ -75,7 +73,7 @@ int size(string text)
 
 // QuickSort
 
-double Partition(vector<int>& vec1, int low, int high)
+int Partition(vector<int>& vec1, int low, int high)
 {
 	int pivot = vec1[high], i = (low - 1);
 
@@ -155,10 +153,10 @@ bool BinarySearch(vector<int> vec1, int data)
 		{
 			vector<int> vec3;
 
-			for (int i = (length / 2); i < length - 1; i++)
-			{
-				vec3.push_back(vec1[i]);
-			}
+				for (int i = (length / 2); i < length - 1; i++)
+				{
+					vec3.push_back(vec1[i]);
+				}
 
 			return BinarySearch(vec3, data);
 		}
@@ -190,15 +188,15 @@ bool BinarySearch(vector<int> vec1, int data)
 		{
 			vector<int> vec3;
 
-			for (int i = high; i < length; i++)
-			{
-				vec3.push_back(vec1[i]);
-			}
+				for (int i = high; i < length; i++)
+				{
+					vec3.push_back(vec1[i]);
+				}
 
-			return BinarySearch(vec3, data);
+				return BinarySearch(vec3, data);
 		}
 	}
-
+	
 }
 
 // Merge Sort
@@ -206,6 +204,8 @@ bool BinarySearch(vector<int> vec1, int data)
 struct SplitVec {
 	vector<int> vec1, vec2;
 };
+
+// In hind sight MergeSort would have been much easier with an array
 
 vector<int> Merge(SplitVec& vec)
 {
@@ -307,12 +307,10 @@ vector<int> Merge(SplitVec& vec)
 				{
 					if (*k > vec.vec1[i])
 					{
-						vec3.emplace(k, vec.vec1[i]);
-						k++;
-						vec3.emplace(k, vec.vec2[j]);
+						vec3.insert(k, 2, vec.vec1[i]);
 						i++;
 						j++;
-						len3++;
+						len3 = len3 + 2;
 					}
 					else
 					{
@@ -323,10 +321,11 @@ vector<int> Merge(SplitVec& vec)
 							{
 								vec3.push_back(vec.vec2[j]);
 								vec3.push_back(vec.vec1[i]);
-								len3 + 2;
 								i++;
 								j++;
-								L + 2;
+								len3++;
+								len3++;
+								L = len3;
 								break;
 							}
 
@@ -334,25 +333,21 @@ vector<int> Merge(SplitVec& vec)
 						}
 						if (L != len3)
 						{
-							vec3.emplace(k, vec.vec1[i]);
-							k++;
-							vec3.emplace(k, vec.vec2[j]);
+							vec3.insert(k, 2, vec.vec1[i]);
 							i++;
 							j++;
-							len3 + 2;
-
+							len3++;
 						}
 					}
 				}
 				else if (len3 == 0)
 				{
-					vec3.emplace(k, vec.vec1[i]);
-					len3++;
-					k++;
-					vec3.emplace(k, vec.vec2[j]);
-					len3++;
+					vec3.push_back(vec.vec2[j]);
+					vec3.push_back(vec.vec1[i]);
 					i++;
 					j++;
+					len3++;
+					len3++;
 				}
 			}
 		}
@@ -375,9 +370,10 @@ vector<int> Merge(SplitVec& vec)
 						len3++;
 						L++;
 						i++;
+						L = len3;
 						break;
 					}
-					k++;
+						k++;
 				}
 				if (L != len3)
 				{
@@ -406,6 +402,7 @@ vector<int> Merge(SplitVec& vec)
 						len3++;
 						j++;
 						L++;
+						L = len3;
 						break;
 					}
 					k++;
@@ -420,7 +417,12 @@ vector<int> Merge(SplitVec& vec)
 		}
 		else if (len1 < len2 && !((len1) > i && len2 > j))
 		{
-			if (*k > vec.vec2[j])
+			if (len3 == 0)
+			{
+				vec3.push_back(vec.vec2[j]);
+				len3++;
+			}
+			else if (*k > vec.vec2[j])
 			{
 				vec3.emplace(k, vec.vec2[j]);
 				j++;
@@ -437,6 +439,7 @@ vector<int> Merge(SplitVec& vec)
 						len3++;
 						j++;
 						L++;
+						L = len3;
 						break;
 					}
 					k++;
@@ -513,51 +516,190 @@ vector<int> MergeSort(vector<int>& vec1)
 
 // Hash()
 
-struct chainBucket {
-	bool collision;//whether or not there was a collision at this field
-	List* bucketList;
-};
-
-class HashTable {
-private:
-	chainBucket Table[79];
-public:
-	unsigned int collisions;
-	void addToTable(string text);
-};
-
 unsigned int Hash(string word)
 {
 	int length = size(word);
+	unsigned int h;
+
 	if (length < 6)
 	{
 		cout << "The min string length is 6, please enter a longer string" << endl;
 		return 0;
 	}
-	unsigned int hash = ((word[0]) + (3 * word[1]) + (5 * word[2]) + (7 * word[3]) + (11 * word[4]) + (13 * word[5]));
-	return hash;
+	if ((word[0] == 'a') || (word[0] = 'A'))
+	{
+		h = ((1 * word[0]) + (3 * word[1]) + (5 * word[2]) + (7 * word[3]) + (11 * word[4]) + (13 * word[5]));
+	}
+	else if ((word[0] == 'b') || (word[0] = 'B'))
+	{
+		h = ((17 * word[0]) + (19 * word[1]) + (23 * word[2]) + (29 * word[3]) + (31 * word[4]) + (37 * word[5]));
+	}
+	else if ((word[0] == 'c') || (word[0] = 'C'))
+	{
+		h = ((41 * word[0]) + (43 * word[1]) + (47 * word[2]) + (53 * word[3]) + (59 * word[4]) + (61 * word[5]));
+	}
+	else if ((word[0] == 'd') || (word[0] = 'D'))
+	{
+		h = ((67 * word[0]) + (71 * word[1]) + (73 * word[2]) + (79 * word[3]) + (83 * word[4]) + (89 * word[5]));
+	}
+	else if ((word[0] == 'e') || (word[0] = 'E'))
+	{
+		h = ((97 * word[0]) + (101 * word[1]) + (103 * word[2]) + (107 * word[3]) + (109 * word[4]) + (113 * word[5]));
+	}
+	else if ((word[0] == 'f') || (word[0] = 'F'))
+	{
+		h = ((127 * word[0]) + (131 * word[1]) + (137 * word[2]) + (139 * word[3]) + (149 * word[4]) + (151 * word[5]));
+	}
+	else if ((word[0] == 'g') || (word[0] = 'G'))
+	{
+		h = ((151 * word[0]) + (113 * word[1]) + (89 * word[2]) + (61 * word[3]) + (37 * word[4]) + (13 * word[5]));
+	}
+	else if ((word[0] == 'h') || (word[0] = 'H'))
+	{
+		h = ((149 * word[0]) + (109 * word[1]) + (83 * word[2]) + (59 * word[3]) + (31 * word[4]) + (11 * word[5]));
+	}
+	else if ((word[0] == 'i') || (word[0] = 'I'))
+	{
+		h = ((139 * word[0]) + (107 * word[1]) + (79 * word[2]) + (53 * word[3]) + (29 * word[4]) + (7 * word[5]));
+	}
+	else if ((word[0] == 'j') || (word[0] = 'J'))
+	{
+		h = ((137 * word[0]) + (103 * word[1]) + (73 * word[2]) + (47 * word[3]) + (23 * word[4]) + (5 * word[5]));
+	}
+	else if ((word[0] == 'k') || (word[0] = 'K'))
+	{
+		h = ((131 * word[0]) + (101 * word[1]) + (71 * word[2]) + (43 * word[3]) + (19 * word[4]) + (3 * word[5]));
+	}
+	else if ((word[0] == 'l') || (word[0] = 'L'))
+	{
+		h = ((127 * word[0]) + (97 * word[1]) + (67 * word[2]) + (41 * word[3]) + (17 * word[4]) + (1 * word[5]));
+	}
+	else if ((word[0] == 'm') || (word[0] = 'M'))
+	{
+		h = ((1 * word[5]) + (3 * word[4]) + (5 * word[3]) + (7 * word[2]) + (11 * word[1]) + (13 * word[0]));
+	}
+	else if ((word[0] == 'n') || (word[0] = 'N'))
+	{
+		h = ((17 * word[5]) + (19 * word[4]) + (23 * word[3]) + (29 * word[2]) + (31 * word[1]) + (37 * word[0]));
+	}
+	else if ((word[0] == 'o') || (word[0] = 'O'))
+	{
+		h = ((41 * word[5]) + (43 * word[4]) + (47 * word[3]) + (53 * word[2]) + (59 * word[1]) + (61 * word[0]));
+	}
+	else if ((word[0] == 'p') || (word[0] = 'P'))
+	{
+		h = ((67 * word[5]) + (71 * word[4]) + (73 * word[3]) + (79 * word[2]) + (83 * word[1]) + (89 * word[0]));
+	}
+	else if ((word[0] == 'q') || (word[0] = 'Q'))
+	{
+		h = ((97 * word[5]) + (101 * word[4]) + (103 * word[3]) + (107 * word[2]) + (109 * word[1]) + (113 * word[0]));
+	}
+	else if ((word[0] == 'r') || (word[0] = 'R'))
+	{
+		h = ((127 * word[5]) + (131 * word[4]) + (137 * word[3]) + (139 * word[2]) + (149 * word[1]) + (151 * word[0]));
+	}
+	else if ((word[0] == 's') || (word[0] = 'S'))
+	{
+		h = ((151 * word[5]) + (113 * word[4]) + (89 * word[3]) + (61 * word[2]) + (37 * word[1]) + (13 * word[0]));
+	}
+	else if ((word[0] == 't') || (word[0] = 'T'))
+	{
+		h = ((149 * word[5]) + (109 * word[4]) + (83 * word[3]) + (59 * word[2]) + (31 * word[1]) + (11 * word[0]));
+	}
+	else if ((word[0] == 'u') || (word[0] = 'U'))
+	{
+		h = ((139 * word[5]) + (107 * word[4]) + (79 * word[3]) + (53 * word[2]) + (29 * word[1]) + (7 * word[0]));
+	}
+	else if ((word[0] == 'v') || (word[0] = 'V'))
+	{
+		h = ((137 * word[5]) + (103 * word[4]) + (73 * word[3]) + (47 * word[2]) + (23 * word[1]) + (5 * word[0]));
+	}
+	else if ((word[0] == 'w') || (word[0] = 'W'))
+	{
+		h = ((131 * word[5]) + (101 * word[4]) + (71 * word[3]) + (43 * word[2]) + (19 * word[1]) + (3 * word[0]));
+	}
+	else if ((word[0] == 'x') || (word[0] = 'X'))
+	{
+		h = ((127 * word[5]) + (97 * word[4]) + (67 * word[3]) + (41 * word[2]) + (17 * word[1]) + (1 * word[0]));
+	}
+	else if ((word[0] == 'y') || (word[0] = 'Y'))
+	{
+		h = ((157 * word[5]) + (163 * word[4]) + (167 * word[3]) + (173 * word[2]) + (179 * word[1]) + (181 * word[0]));
+	}
+	else if ((word[0] == 'z') || (word[0] = 'Z'))
+	{
+		h = ((191 * word[5]) + (193 * word[4]) + (197 * word[3]) + (199 * word[2]) + (211 * word[1]) + (223 * word[0]));
+	}
+
+	return h;
 }
+
+struct chainBucket {
+	bool collision;
+	unsigned int Hash;
+	List* bucketList = new List;
+};
+
+class HashTable {
+private:
+
+	chainBucket Table[79];
+
+public:
+
+	unsigned int collisions;
+
+	void addToTable(string text);
+	void printTable();
+};
 
 void HashTable::addToTable(string text)
 {
-	unsigned int hash = Hash(text),  key = Hash(text) % 79;
+	unsigned int hash = Hash(text), key = Hash(text) % 79;
 
-	if(hash == 0)
+	if (hash == 0)
 	{
-		   return;
+		return;
 	}
-	if (Table[key].bucketList->empty())
+	if (Table[key].Hash != hash)
 	{
 		chainBucket bucket;
 		bucket.collision = false;
+		bucket.Hash = hash;
 		bucket.bucketList = new List;
-		bucket.bucketList->insertAsFirst(Hash(text));
+		bucket.bucketList->insertAsFirst(text);
 		Table[key] = bucket;
 	}
 	else
 	{
-		Table[key].collision == true;
-		Table[key].bucketList->insertAsLast(hash);
+		Table[key].collision = true;
+		Table[key].bucketList->insertAsLast(text);
 		collisions++;
+	}
+}
+
+
+// Help from lab 10 HashMaps
+
+void HashTable::printTable()
+{
+	cout << "\nKey:[HASHVAL] Bucket:[<COLLISION?>]\n";
+	cout << ">>  LinkedList  \n\n";
+
+	for (const auto& n : Table) 
+	{
+		List* chain = new List;
+		chain = n.bucketList;
+
+		if (n.Hash != 3435973836)
+		{
+			cout << "Key:[" << n.Hash << "] Bucket:[<" << n.collision << ">]";
+			if (n.collision)
+			{
+				chain->print();
+			}
+				cout << "\n";
+		}
+
 	}
 }
